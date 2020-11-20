bf30db15ddeb9e8e46d4fa3ee715cdfb8813046c
chris60303 <chris60303@gmail.com>
2020-11-20 16:29:15 +0800

    count_avg

--- count_avg.py
+++ count_avg.py
@@ -1,5 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 if __name__=="__main__":
-    f = open('my_test.txt','r')
+    f = open('my_test.txt', "r")
+    f.readlines()
+    total_count = 0
+    total_sum = 0
     for line in f.readlines():
+        total_count+=1
         print(line)
+        total_sum += line
+    print("finish reading")
+    print("avg is " + total_sum/total_count)
     f.close
