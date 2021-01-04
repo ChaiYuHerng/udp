6cf5e4aec38504e94d0dace9098a0c99ce01fbf1
chris60303 <chris60303@gmail.com>
2021-01-04 18:24:55 +0800

    count debug

--- count_avg.py
+++ count_avg.py
@@ -2,13 +2,13 @@
 # -*- coding: utf-8 -*-
 if __name__=="__main__":
     f = open('my_test.txt', "r")
-    f.readlines()
     total_count = 0
     total_sum = 0
     for line in f.readlines():
-        total_count+=1
         print(line)
-        total_sum += line
+        if total_count >0 :
+            total_sum += float(line)
+        total_count+=1
     print("finish reading")
-    print("avg is " + total_sum/total_count)
+    print("avg is " + str(total_sum/total_count))
     f.close
